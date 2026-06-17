588710c0cfa4770626579bcdf5d9e63eb1910f16
RaghavendraT <torriraghub4u@gmail.com>
2026-06-17 13:59:35 +0530

    add_files

---
+++ story_content/triggers.js
@@ -0,0 +1,29 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5pAdSB4hzl1":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+};
